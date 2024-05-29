daf221707b2cbd42f28eb4d5d3e0a0245d5d3e1e
zhipenglin <sunandmoon001@163.com>
2024-05-29 11:12:15 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,41 @@
+const fp = require('fastify-plugin');
+const autoload = require('@fastify/autoload');
+const fs = require('fs');
+module.exports = fp(
+  async (fastify, options) => {
+    const {
+      name: baseName,
+      modules,
+      options: otherOptions
+    } = Object.assign(
+      {
+        modules: {}
+      },
+      options
+    );
+    const proxy = {};
+    for (let [name, module] of modules) {
+      const stat = typeof module === 'string' && (await fs.promises.stat(module));
+      (() => {
+        if (stat && stat.isDirectory()) {
+          proxy[name] = {};
+          fastify.register(autoload, {
+            dir: module,
+            options: otherOptions
+          });
+          return;
+        }
+        if (stat && stat.isFile()) {
+          proxy[name] = {};
+          fastify.register(require(module), otherOptions);
+          return;
+        }
+        proxy[name] = module;
+      })();
+    }
+    fastify.decorate(baseName, proxy);
+  },
+  {
+    name: 'fastify-namespace'
+  }
+);
