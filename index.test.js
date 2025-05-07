2f886ef3bc9b674d384daf3fc4473a4158dc105e
zhipenglin <zhipeng.lin@leapin.io>
2025-05-07 13:39:30 +0800

    发布新版本

--- index.test.js
+++ index.test.js
@@ -1,11 +1,12 @@
-import Fastify from 'fastify';
-import { test } from 'node:test';
-import namespace from './index.js';
+const Fastify = require('fastify');
+const { test } = require('node:test');
+const namespace = require('./index');
 
 test('Test namespace', async t => {
   const fastify = Fastify();
   fastify.register(namespace, {
-    name: 'testModule', modules: [['utils', '123456']]
+    name: 'testModule',
+    modules: [['utils', '123456']]
   });
   await fastify.ready();
   t.assert.strictEqual(fastify.testModule.utils, '123456');
